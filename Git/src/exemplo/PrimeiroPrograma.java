1808c9f64368f089677538817a6159d7cf0ad037
Fernanda Saliba <73433179+FernandaSaliba@users.noreply.github.com>
2023-03-15 00:26:23 -0300

    Treinamento
    Treinamento dio

---
+++ Git/src/exemplo/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package exemplo;
+
+import exemplo.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+
+	}
+
+}
+
